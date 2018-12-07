c407b1ad304675e0d57f56d13acdd4a7e892b60c
David Moore <dpm4891@yahoo.com>
2018-12-07 12:58:25 -0600

    Chapter 11 done. Routing in place

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -3,10 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pm-root',
   template: `
+    <nav class='navbar navbar-expand navbar-light bg-light'>
+      <a class='navbar-brand'>{{pageTitle}}</a>
+      <ul class='nav van-pills'>
+        <li><a class='nav-link' [routerLink]="['/welcome']">Home</a></li>
+        <li><a class='nav-link' [routerLink]="['/products']">Product List</a></li>
+      </ul>
+    </nav>
+    <div class='container'>
+      <router-outlet></router-outlet>
     <div>
-      <h1>{{pageTitle}}</h1>
-      <pm-products></pm-products>
-    </div>
     `
 })
 export class AppComponent {
